9042010f508c8bebf3241dc9af68cf52e8db898b
Michael A. Perlin <mika.perlin@gmail.com>
2018-12-13 17:26:12 -0700

    use pactl in place of pamixer

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,24 +1,47 @@
 #!/usr/bin/env sh
 
 delta=5
-medium=50
+medium=40
 
-if [ $1 = dec ]; then
-  pamixer --unmute
-  pamixer --decrease $delta
-elif [ $1 = inc ]; then
-  pamixer --unmute
-  pamixer --increase $delta
-elif [ $1 = min ]; then
-  pamixer --set-volume 0
-elif [ $1 = max ]; then
-  pamixer --set-volume 100
-elif [ $1 = med ]; then
-  pamixer --set-volume $medium
-elif [ $1 = on ]; then
-  pamixer --unmute
-elif [ $1 = off ]; then
-  pamixer --mute
-elif [ $1 = toggle ]; then
-  pamixer --toggle-mute
+device=sink
+device_num=0
+cmd=$1
+
+if [ $1 = mic ]; then
+  device=source
+  device_num=1
+  cmd=$2
+fi
+
+if [ $cmd = dec ]; then
+  pactl set-$device-mute $device_num 0
+  pactl set-$device-volume $device_num -$delta%
+elif [ $cmd = inc ]; then
+  pactl set-$device-mute $device_num 0
+  pactl set-$device-volume $device_num +$delta%
+elif [ $cmd = min ]; then
+  pactl set-$device-volume $device_num 0
+elif [ $cmd = max ]; then
+  pactl set-$device-volume $device_num 100%
+elif [ $cmd = med ]; then
+  pactl set-$device-volume $device_num $medium%
+elif [ $cmd = on ]; then
+  pactl set-$device-mute $device_num 0
+elif [ $cmd = off ]; then
+  pactl set-$device-mute $device_num 1
+elif [ $cmd = toggle ]; then
+  pactl set-$device-mute $device_num toggle
+fi
+
+if [ $device = source ]; then
+  if [ $cmd = toggle ] || [ $cmd = on ] || [ $cmd = off ]; then
+    mute=`pactl list sources | grep Mute | tail -n 1 | awk '{print $2}'`
+    if [ $mute = yes ]; then
+      notify-send -t 1 "mic off"
+    elif [ $mute = no ]; then
+      notify-send -t 1 "mic on"
+    else
+      notify-send -t 1 "mic status unknown"
+    fi
+  fi
 fi
