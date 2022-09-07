67bbc4ba49345c327594dd7e92abffa4760b205a
Michael A. Perlin <mika.perlin@gmail.com>
2022-09-07 11:29:30 -0500

    update emacs config file

--- configs/home/perlinm/.emacs.d/init.el
+++ configs/home/perlinm/.emacs.d/init.el
@@ -1,11 +1,35 @@
 ;; -------------------------------------------------------------------------------------
 ;; Package declaration and configuration
-(require 'package)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(package-refresh-contents)
-(package-initialize)
+;;(require 'package)
+;;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+;;;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;;(package-refresh-contents)
+;;(package-initialize)
+
+;; <leaf-install-code>
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
+
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+;; </leaf-install-code>
 
 (leaf recentf
   :config
