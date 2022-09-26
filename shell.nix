d57262d7f1349c6db809b6f4d0e648044ffb15b7
Michael A. Perlin <mika.perlin@gmail.com>
2022-09-25 21:57:18 -0500

    update ~/scirpts and ~/bin link paths

--- shell.nix
+++ shell.nix
@@ -69,8 +69,8 @@
 
   activation = {
     makeSymbolicLinks = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      $DRY_RUN_CMD ln -sTf $VERBOSE_ARG $HOME/git/bin $HOME/bin
-      $DRY_RUN_CMD ln -sTf $VERBOSE_ARG $HOME/git/scripts $HOME/scripts
+      $DRY_RUN_CMD ln -sTf $VERBOSE_ARG $HOME/nix/bin $HOME/bin
+      $DRY_RUN_CMD ln -sTf $VERBOSE_ARG $HOME/nix/scripts $HOME/scripts
     '';
   };
 }
