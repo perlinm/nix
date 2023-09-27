002f4685f8af2000cf8a9151dc39136565b20d5a
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-26 21:17:11 -0500

    clean up configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,6 @@
-{ config, pkgs, ... }:
-
-let sway-fixes = import ./sway-fixes.nix { inherit pkgs; };
-in {
+{ pkgs, ... }:
+# let sway-fixes = import ./sway-fixes.nix { inherit pkgs; };
+{
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
