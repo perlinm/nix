4626270b9ea2e4a4ded64757aa50d2610eb20b6e
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-26 21:37:12 -0500

    move mathematica override

--- packages.nix
+++ packages.nix
@@ -3,20 +3,6 @@ let
   # install unstable packages with unstable.<PACKAGE-NAME>
   unstable = import <nixos-unstable> { config.allowUnfree = true; };
 
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/science/math/mathematica/default.nix
-  mathematica-13-2-1 = unstable.mathematica.override {
-    source = pkgs.requireFile {
-      name = "Mathematica_13.2.1_BNDL_LINUX.sh";
-      # Get this hash via a command similar to this:
-      # nix-store --query --hash $(nix store add-path Mathematica_13.2.1_BNDL_LINUX.sh --name 'Mathematica_13.2.1_BNDL_LINUX.sh')
-      sha256 = "070ybhgskk3fw8c6fgqs4lq9252ds6585cqdd5as94hj55vjibmq";
-      message = ''
-        Your override for Mathematica includes a different src for the installer, and it is missing.
-      '';
-      hashMode = "recursive";
-    };
-  };
-
   languages = with pkgs; [
     cargo
     gcc
@@ -153,6 +139,20 @@ let
     lynx # text-based browser
     protobuf # for protoc command
   ];
+
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/science/math/mathematica/default.nix
+  mathematica-13-2-1 = unstable.mathematica.override {
+    source = pkgs.requireFile {
+      name = "Mathematica_13.2.1_BNDL_LINUX.sh";
+      # Get this hash via a command similar to:
+      # nix-store --query --hash $(nix store add-path Mathematica_13.2.1_BNDL_LINUX.sh --name 'Mathematica_13.2.1_BNDL_LINUX.sh')
+      sha256 = "070ybhgskk3fw8c6fgqs4lq9252ds6585cqdd5as94hj55vjibmq";
+      message = ''
+        Your override for Mathematica includes a different src for the installer, and it is missing.
+      '';
+      hashMode = "recursive";
+    };
+  };
 
 in {
   home.packages = console-utilities ++ languages ++ python ++ fonts-icons-themes
