77a7bb25c5e90252877530f925b15034da777611
Michael A. Perlin <mika.perlin@gmail.com>
2023-10-25 18:05:25 -0500

    minor cleanup of luks.nix

--- luks.nix
+++ luks.nix
@@ -1,11 +1,12 @@
-{
+let luks-swap-uuid = "6584249e-94c5-4559-a9e8-3654b2b164ae";
+in {
   boot.initrd = {
-    # setup keyfile for encrypted hard drive
+    # setup keyfile for a luks-encrypted hard drive
     secrets = { "/crypto_keyfile.bin" = null; };
 
-    # enable swap on encrypted disks
-    luks.devices."luks-6584249e-94c5-4559-a9e8-3654b2b164ae" = {
-      device = "/dev/disk/by-uuid/6584249e-94c5-4559-a9e8-3654b2b164ae";
+    # enable swap on a luks-encrypted hard drive
+    luks.devices."luks-${luks-swap-uuid}" = {
+      device = "/dev/disk/by-uuid/${luks-swap-uuid}";
       keyFile = "/crypto_keyfile.bin";
     };
   };
