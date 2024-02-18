3f738c321183f5127d0eab6ff6cd9e0d4098cc5b
Michael A. Perlin <mika.perlin@gmail.com>
2024-02-18 16:07:23 -0600

    fix issue with pasting into zsh w/ autosuggest

--- programs.nix
+++ programs.nix
@@ -51,6 +51,18 @@ in {
 
         eval "$(starship init zsh)"
         export PATH=$HOME/bin:$PATH
+
+        # This speeds up pasting w/ autosuggest
+        # https://github.com/zsh-users/zsh-autosuggestions/issues/238
+        pasteinit() {
+          OLD_SELF_INSERT=$\{$\{(s.:.)widgets[self-insert]\}[2,3]\}
+          zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+        }
+        pastefinish() {
+          zle -N self-insert $OLD_SELF_INSERT
+        }
+        zstyle :bracketed-paste-magic paste-init pasteinit
+        zstyle :bracketed-paste-magic paste-finish pastefinish
       '';
       sessionVariables = shell.home.sessionVariables;
     };
