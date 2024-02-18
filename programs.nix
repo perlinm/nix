9e4c032642d4f23a37b55fbecbdcfda104e38471
Michael A. Perlin <mika.perlin@gmail.com>
2024-02-18 16:26:13 -0600

    fix escape character

--- programs.nix
+++ programs.nix
@@ -55,7 +55,7 @@ in {
         # This speeds up pasting w/ autosuggest
         # https://github.com/zsh-users/zsh-autosuggestions/issues/238
         pasteinit() {
-          OLD_SELF_INSERT=$\{$\{(s.:.)widgets[self-insert]\}[2,3]\}
+          OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
           zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
         }
         pastefinish() {
